15df646780389548479d56fd6aedee216c861c53
forpi <francisco.ortola.piles@gmail.com>
2016-09-22 10:06:49 +0200

    Ya esta modificado

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//fichero index.php
+//creado por Paco Gomez
+?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+        echo "Francisco <br>"; 
+        echo "Ortola <br>";
+        echo "Piles <br>";
+      ?>
+  </head>
+  <body>
+</html>
